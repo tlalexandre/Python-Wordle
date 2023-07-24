0101fba6b578bc4b0eeb436188fe2d94fd6ef503
BanditCook <tanguy.lalexandre@gmail.com>
2023-07-24 21:00:02 +0100

    Fix Bug of same letter twice in the word uncorreclty displayed

--- run.py
+++ run.py
@@ -6,6 +6,14 @@ from colorama import Fore, Back, init
 import requests
 init(autoreset=True)
 
+G = Back.LIGHTGREEN_EX
+R = Back.RED
+B = Back.CYAN
+Y = Back.YELLOW
+W = Back.WHITE
+M = Back.MAGENTA
+ENDC = Back.RESET
+
 
 def fetch_api_data(api_url):
     try:
@@ -31,13 +39,13 @@ api_url = 'https://random-word-api.vercel.app/api?words=1&length=5&type=uppercas
 
 
 def intro_display():
-    print(Fore.CYAN+"Welcome to Python Wordle\n")
-    print(Fore.BLUE+"Press Enter to start")
-    print(Fore.GREEN+"The rules are simple: Guess the word on the screen in the less tries possible.\n Try to input differents letters and see if they exist in the word.\n If the letter exists in word but is misplaced it will turn "+(Back.YELLOW+"YELLOW\n"))
-    print(Fore.GREEN+"If the letter doesn't exist in the word, it will turn " +
-          (Back.RED+"RED\n"))
+    print(B+"Welcome to Python Wordle\n")
+    print(B+"Press Enter to start")
+    print(G+"The rules are simple: Guess the word on the screen in the less tries possible.\n Try to input differents letters and see if they exist in the word.\n If the letter exists in word but is misplaced it will turn "+(Y+"YELLOW\n"))
+    print(G+"If the letter doesn't exist in the word, it will turn " +
+          (R+"RED\n"))
 
-    print(Fore.WHITE+"Good Luck !")
+    print(W+"Good Luck !")
 
 
 def user_word():
@@ -57,7 +65,7 @@ def add_guesses(secret_letters, user_letters):
 
 
 def victory(guess, secret):
-    print(Back.MAGENTA +
+    print(M +
           f"Well Play ! You guessed in only {guess} times to find the word: {secret}")
 
 
@@ -69,25 +77,29 @@ def compare_letters(secret_letters, user_letters):
     If they have different positions but same letters, return an answer right letter wrong position. 
     If completely different, return answer, does not exist in the word
     '''
-
+    secret_letters = list(secret_letters)
+    user_letters = list(user_letters)
     common_letters = set(secret_letters) & set(user_letters)
     if secret_letters == user_letters:
-        victory
+        victory(guess, secret)
     else:
         for letter in user_letters:
             if letter in common_letters:
                 position_secret = secret_letters.index(letter)
                 position_user = user_letters.index(letter)
                 if position_secret == position_user:
-                    print(Back.GREEN +
-                          f"{letter}")
+
+                    print(G+f"{letter}"+ENDC, end="")
+                    secret_letters[position_secret] = None
+                    user_letters[position_user] = None
+
                 else:
 
-                    print(Back.YELLOW +
-                          f"{letter}")
+                    print(Y+f"{letter}"+ENDC, end="")
+                    secret_letters[position_secret] = None
             else:
-
-                print(Back.RED+f"{letter}")
+                print(R+f"{letter}"+ENDC, end="")
+    print("\n")
 
 
 secret = fetch_api_data(api_url)
