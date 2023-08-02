e2caebd6b7a41efb13533d741362e64baafc3b75
BanditCook <tanguy.lalexandre@gmail.com>
2023-08-02 13:06:16 +0100

    Fix the bug of duplicate letters !

--- run.py
+++ run.py
@@ -109,82 +109,43 @@ def victory(guess, secret):
 wrong_letters = set()
 
 
-# def compare_letters(secret_letters, user_letters, wrong_letters):
-#     '''
-#     Creates a set with the letters from the user and the one from the secret word.
-#     Iterates through the user_letters, if the letter exists , it triggers the second condition checking the position of the letters.
-#     If they're identical, return an answer about right letter and right position
-#     If they have different positions but same letters, return an answer right letter wrong position.
-#     If completely different, return answer, does not exist in the word
-#     '''
-
-#     secret_letters = list(secret_letters)
-#     user_letters = list(user_letters)
-#     common_letters = set(secret_letters) & set(user_letters)
-
-#     correct_positions = set()
-#     misplaced_positions = set()
-#     for letter in user_letters:
-#         if letter in common_letters:
-#             position_secret = [i for i, x in enumerate(
-#                 secret_letters) if x == letter]
-#             position_user = [i for i, x in enumerate(
-#                 user_letters) if x == letter]
-#             for pos_secret in position_secret:
-#                 for pos_user in position_user:
-#                     if pos_secret == pos_user:
-#                         correct_positions.add(pos_user)
-#                     else:
-#                         misplaced_positions.add(pos_user)
-#     for i, letter in enumerate(user_letters):
-#         if letter in common_letters:
-#             if i in correct_positions:
-#                 print(BG+f"{letter}"+ENDC, end="")
-#             elif i in misplaced_positions:
-#                 print(BY+f"{letter}"+ENDC, end="")
-#         else:
-#             print(BR + f"{letter}"+ENDC, end="")
-#             wrong_letters.add(letter)
-#     print("\n")
-
-#     print(FW+f"Here is a list of the letters you already tried and are wrong:",
-#           BR + f"{wrong_letters}")
-#     print("\n")
-#     return wrong_letters
-
-def compare_letters(secret_letters, user_letters, wrong_letters):
+def compare_letters(user_letters, secret_letters, wrong_letters):
     secret_letters = list(secret_letters)
     user_letters = list(user_letters)
-    common_letters = set(secret_letters) & set(user_letters)
+    correct_positions = []
+    misplaced_positions = []
+    wrong_positions = []
+    user_counts = {}
+    secret_counts = {}
 
-    correct_positions = set()
-    potential_misplaced_positions = set()
+    for letter in secret_letters:
+        secret_counts[letter] = secret_counts.get(letter, 0)+1
 
-    # Find correct letters in the right positions
+    for letter in user_letters:
+        user_counts[letter] = user_counts.get(letter, 0)+1
+
     for i, letter in enumerate(user_letters):
         if letter == secret_letters[i]:
-            correct_positions.add(i)
+            correct_positions.append(i)
 
-    # Find potential positions of misplaced letters
     for i, letter in enumerate(user_letters):
-        if letter in common_letters and i not in correct_positions:
-            position_secret = [pos for pos, x in enumerate(
-                secret_letters) if x == letter]
-            potential_misplaced_positions |= set(position_secret)
+        if letter in secret_letters and i not in correct_positions:
+            misplaced_positions.append(i)
 
-    # Compare letters and positions
     for i, letter in enumerate(user_letters):
-        if i in correct_positions:
-            print(BG + f"{letter}" + ENDC, end="")
-        elif i in potential_misplaced_positions:
-            print(BY + f"{letter}" + ENDC, end="")
-        else:
-            print(BR + f"{letter}" + ENDC, end="")
+        if letter not in secret_letters or user_counts[letter] > secret_counts[letter]:
+            wrong_positions.append(i)
             wrong_letters.add(letter)
 
+    for i, letter in enumerate(user_letters):
+        if i in correct_positions:
+            print(BG+f"{letter}")
+        elif i in misplaced_positions and i not in wrong_positions:
+            print(BY+f"{letter}")
+        elif i in wrong_positions:
+            print(BR+f"{letter}")
     print("\n")
-    print(FW + f"Here is a list of the letters you already tried and are wrong:",
-          BR + f"{wrong_letters}")
+    print(BR+f"Here's the letters you tried and were wrong: {wrong_letters}")
     print("\n")
     return wrong_letters
 
@@ -204,6 +165,8 @@ def main_game():
     word_length = ask_word_length()
     api_url = f'https://random-word-api.vercel.app/api?words=1&length={word_length}&type=uppercase'
     secret = fetch_api_data(api_url)
+    # secret = "CIVIL"
+    print(secret)
     guess = 0
     while True:
         user = user_word()
@@ -214,8 +177,9 @@ def main_game():
         user_letters = split_string(user)
         if check_length(user_letters, word_length):
             if check_real_word(user):
-                compare_letters(secret_letters, user_letters,
-                                wrong_letters)
+                compare_letters(
+                    user_letters, secret_letters, wrong_letters)
+
                 if secret_letters == user_letters:
                     print(
                         BM+f"Well Play, you guessed the word {secret} in only{guess}")
